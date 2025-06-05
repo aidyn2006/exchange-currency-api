a9a07cc564794cf10b18d55a7a03cff457f23449
Aidyn <nurlan.aydin06@gmail.com>
2025-06-05 18:31:55 +0500

    Default dependency

---
+++ src/main/java/org/example/technicaltaskexchange/TechnicalTaskExchangeApplication.java
@@ -0,0 +1,13 @@
+package org.example.technicaltaskexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTaskExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalTaskExchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/technicaltaskexchange/TechnicalTaskExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.technicaltaskexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTaskExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
